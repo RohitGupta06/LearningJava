200704c19ccd494ff9b8a6928c4fcc73c76dc30f
Rohit <rohit@ROHITGs-MacBook-Pro.local>
2023-07-31 11:36:33 +0530

    Created jump statements package

---
+++ java-workspace/C-Control Statements/src/com/java/jumpstatements/Break.java
@@ -0,0 +1,10 @@
+package com.java.jumpstatements;
+
+public class Break {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ java-workspace/C-Control Statements/src/com/java/jumpstatements/BreakAsGoto.java
@@ -0,0 +1,10 @@
+package com.java.jumpstatements;
+
+public class BreakAsGoto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
